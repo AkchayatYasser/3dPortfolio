d99bd1492e9c4155bedd209521e3eb4ecd822607
Yasser Akchayat <matriix@Yassers-MacBook-Pro.local>
2024-07-12 00:40:38 +0100

    chi 7aja

---
+++ src/components/About.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a software engineer with expertise in JavaScript, React.js, and Node.js. 
+        In addition to my strong web development skills, I also possess knowledge in 
+        Machine Learning. I have experience working with machine learning frameworks 
+        and libraries such as TensorFlow and scikit-learn. This allows me to leverage 
+        data-driven approaches and develop intelligent applications that can make predictions 
+        and provide valuable insights. Whether it's building robust web applications or incorporating 
+        Machine Learning capabilities, I am committed to delivering high-quality solutions. 
+        With my understanding of both software engineering and machine learning, I can create innovative 
+        and efficient software experiences that meet your specific needs. 
+        Let's collaborate and turn your ideas into reality!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
